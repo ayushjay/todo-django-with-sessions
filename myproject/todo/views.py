5f7346fac33b2c2f46905aeae1b6559ccfb9c4ef
Ayush J <ayushjayaswal51@gmail.com>
2025-07-31 23:40:15 +0530

    added all files

--- myproject/todo/views.py
+++ myproject/todo/views.py
@@ -1,15 +1,50 @@
 from django.shortcuts import render
 from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 
 def home(request):
-    return render(request, "todo/index.html")
+
+    # Check if there already exists a "tasks" key in our session
+
+    if "tasks" not in request.session:
+
+        # If not, create a new list
+        request.session["tasks"] = []
+
+    return render(request, "todo/index.html", {
+        "tasks": request.session["tasks"]
+    })
 
 # Add a new task:
 def add(request):
-    return render(request, "tasks/add.html", {
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
+            request.session["tasks"] += [task]
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("todo:home"))
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "todo/add.html", {
+                "form": form
+            })
+
+    return render(request, "todo/add.html", {
         "form": NewTaskForm()
     })
